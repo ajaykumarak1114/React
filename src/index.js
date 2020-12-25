2a3a6a7ee649221b8229e217a1ce8442d07d6da4
Ajay <ajaykumarak1114@gmail.com>
2020-12-25 17:11:06 +0530

    Add react props

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 //1. Apply CSS styles to App.jsx component
